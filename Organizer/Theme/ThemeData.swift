2f60d763d0bc5817054a6f5f7f0084d01312ce02
Feruu <yuldashevasabiya@gmail.com>
2024-07-13 12:45:17 +0500

    New brach

---
+++ Organizer/Theme/ThemeData.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeData.swift
+//  Organizer
+//
+//  Created by Feruza Yuldasheva on 7/12/24.
+//
+
+import Foundation
---
+++ Organizer/Theme/ThemeSettings.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeSettings.swift
+//  Organizer
+//
+//  Created by Feruza Yuldasheva on 7/12/24.
+//
+
+import Foundation
